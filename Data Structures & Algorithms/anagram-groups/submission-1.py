e1ab8e454694ad7423ff018680fac44d005d98d8
chew0522 <dcbc0123456789@gmail.com>
2026-08-31 08:46:44 +0800

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:    
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        bucket = defaultdict(list)
+        for s in strs: 
+            sorted_s = "".join(sorted(s))
+            bucket[sorted_s].append(s)
+
+        return list(bucket.values())
+
+
+    
+
+    
